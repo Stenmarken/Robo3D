8de8c3ecd74fc81362c784411263b5c38c285052
Stenmarken <vstenm@kth.se>
2025-03-12 21:29:58 +0100

    Add support for ROADVIEW PCs

---
+++ create/roadview_c/fog/moderate_fog.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python fog_simulation.py \
+            --root_folder root_folder \
+            --dst_folder  dst_folder \
+            --inte_folder  integral_lookup_tables_seg_moderate_0.05beta \
+            --beta  0.05
